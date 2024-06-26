1413acdc70d9a61d0d4fc45457f1f9235222cdc4
drillhill <148098611+drillhill@users.noreply.github.com>
2024-06-27 02:43:08 +0700

    mod: update test_visual.py

--- tests/test_visual.py
+++ tests/test_visual.py
@@ -1,5 +1,8 @@
 import subprocess
 import pytest
+from controller.controller import Controller
+from model.model import Model
+from view.view import View
 
 @pytest.fixture(scope="module")
 def run_visual_command():
@@ -14,4 +17,16 @@ def test_visual_output(run_visual_command):
 
     assert "Commits History Visualization" in run_visual_command, "Title not found"
     assert "*" in run_visual_command, "Graph characters not found"
-    #assert "Author:" in run_visual_command, "Author information not found"
+
+def test_empty_repository(tmpdir):
+    """Asserts a GitCommandError when the repository is empty."""
+    with tmpdir.as_cwd():
+        subprocess.run(["git", "init"])
+        with pytest.raises(GitCommandError, match="No commits found."):
+            subprocess.run(["python", "main.py", "--visual"], capture_output=True, text=True)
+
+def test_invalid_repository(tmpdir):
+    """Asserts a GitCommandError for invalid repository paths."""
+    with tmpdir.as_cwd():
+        with pytest.raises(GitCommandError, match="fatal: not a git repository"):
+            subprocess.run(["python", "main.py", "--visual"], capture_output=True, text=True)
